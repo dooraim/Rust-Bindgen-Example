e4266bdb46c56c1c244c0c4405cdaf6205ea396d
dooraim <dooraim@yahoo.com>
2024-09-28 18:09:33 +0200

    libc: add test library

---
+++ libc/test/test.c
@@ -0,0 +1,7 @@
+#include "include/test2.h"
+#include "include2/test3.h"
+
+// add.c
+int add(int x, int y) {
+    return x + y;
+}
---
+++ libc/test/test.h
@@ -0,0 +1,4 @@
+#include "include/test2.h"
+#include "include2/test3.h"
+
+int add(int x, int y); // function prototype for add.h -- don't forget the semicolon!
